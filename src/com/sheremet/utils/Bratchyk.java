3187c187802b316908f4c9f2aada41d6a38dab80
yarphen <leviatan.ka14@gmail.com>
2015-04-21 15:13:19 +0300

    API creating

--- src/com/sheremet/utils/Bratchyk.java
+++ src/com/sheremet/utils/Bratchyk.java
@@ -3,20 +3,42 @@ package com.sheremet.utils;
 import java.sql.Date;
 
 public class Bratchyk {
-	public String prizvysche;
+	public Bratchyk(Bratchyk b) {
+		dataankety = b.dataankety;
+		datanarodzhennia = b.datanarodzhennia;
+		dataopatronennia = b.dataopatronennia;
+		dataposhanuvannia = b.dataposhanuvannia;
+		datavysviaty = b.datavysviaty;
+		id = b.id;
+		imya = b.imya;
+		kontakty = b.kontakty;
+		patron_id = b.patron_id;
+		pobatkovi = b.pobatkovi;
+		prizvysche = b.prizvysche;
+		rikvstupu = b.rikvstupu;
+		rikvypusku = b.rikvypusku;
+		specialnist = b.specialnist;
+		version_id = b.version_id;
+		
+	}
+	public Bratchyk() {
+		// TODO Auto-generated constructor stub
+	}
+	public Date dataankety;
+	public Date datanarodzhennia;//private
+	public Date dataopatronennia;
+	public Date dataposhanuvannia;
+	public Date datavysviaty;//private
+	public long id;
 	public String imya;
-	public String pobatkovi;
 	public String kontakty;//private
+	public Integer patron_id;
+	public String prizvysche;
+	public String pobatkovi;
 	public String posady;
-	public String specialnist;
 	public Integer rikvstupu;
 	public Integer rikvypusku;
-	public Date datanarodzhennia;//private
-	public Date dataankety;
-	public Date dataopatronennia;
-	public Date datavysviaty;
-	public Date dataposhanuvannia;
-	public Integer patron_id;
+	public String specialnist;
 	public Integer version_id;//private
-	long id;
+	
 }
--- src/com/sheremet/utils/User.java
+++ src/com/sheremet/utils/User.java
@@ -1,10 +1,19 @@
 package com.sheremet.utils;
 
 public class User {
-	
-	String name;
-	String email;
-	String passhash;
-	int permission;
-	long id;
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	public User(User u) {
+		name = u.name;
+		email = u.email;
+		passhash = u.passhash;
+		permission = u.permission;
+		id = u.id;
+	}
+	public String name;
+	public String email;
+	public String passhash;
+	public int permission;
+	public long id;
 }
