d227fdc92ce13a65056e28b45f2cbedf10993ec6
tsyapa <tsyapper14@gmail.com>
2015-04-21 15:13:18 +0300

    add parser

--- src/com/sheremet/tests/SimpleTester.java
+++ src/com/sheremet/tests/SimpleTester.java
@@ -3,6 +3,9 @@ package com.sheremet.tests;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.HashMap;
+import java.util.regex.Pattern;
+
+import com.sheremet.utils.Parser;
 
 public class SimpleTester {
 	public static void main(String[] args) throws IOException {
--- src/com/sheremet/utils/Parser.java
+++ src/com/sheremet/utils/Parser.java
@@ -1,19 +1,28 @@
 package com.sheremet.utils;
 import java.sql.Date;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class Parser {
+	
 	public static HashMap<String, Object> parse(String s) {
-		return null;
-		
-		// TODO Auto-generated method stub
-
+		String[] arr=s.split(Pattern.quote("\\"));
+		HashMap<String, Object> map=new HashMap();
+		String temp[]=new String[2];
+		for(String s1:arr){
+			temp=s1.split(Pattern.quote("="),2);
+			map.put(temp[0], temp[1]);
+		}
+		return map;
 	}
+	
 	public static String unparse(HashMap<String, Object> map) {
-		return null;
-		
-		// TODO Auto-generated method stub
-
+		String res="";
+		Set<String> set=map.keySet();
+		for(String s:set)
+			res+=s+"="+map.get(s)+"\\";
+		return res;
 	}
 	
 	String getType(Object o){
