09e673318d21bb49b68548786f0d1558ecb27a2f
yarphen <leviatan.ka14@gmail.com>
2015-05-04 03:05:35 +0300

    API changing concept

--- src/com/sheremet/client/DBSecureAPI.java
+++ src/com/sheremet/client/DBSecureAPI.java
@@ -4,9 +4,7 @@ import java.net.ConnectException;
 import java.sql.Date;
 import java.util.HashMap;
 
-import com.sheremet.utils.Bratchyk;
-import com.sheremet.utils.Parser;
-import com.sheremet.utils.User;
+import com.sheremet.utils.*;
 
 public class DBSecureAPI {
 	private String access_token;
@@ -19,7 +17,7 @@ public class DBSecureAPI {
 		map.put("password", password);
 		String string  = Parser.unparse(map);
 		connection.send(string, new StringResultHandler() {
-			
+
 			@Override
 			void handle(String s) throws Exception {
 				HashMap<String, Object> map = Parser.parse(s);
@@ -31,196 +29,189 @@ public class DBSecureAPI {
 			}
 		});
 	}
+	public String login(String login, String password){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "login");
+		map.put("login", login);
+		map.put("password", password);
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (String) command.execute(false);
+	}
 	public Bratchyk[] getBratchykChildren(long id){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "getBratchykChildren");
 		map.put("access_token", access_token);
-		map.put("act", "get");
 		map.put("id", id);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk.makeCopy(new Bratchyk(Parser.parse(s)));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (Bratchyk[]) command.execute(true);
 	}
-	public Bratchyk[] getHeadBratchyks(){
+	public Bratchyk[] getHeadBratchyks(){		
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "getHeadBratchyks");
 		map.put("access_token", access_token);
-		map.put("act", "get");
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk.makeCopy(new Bratchyk(Parser.parse(s)));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (Bratchyk[]) command.execute(true);
 	}
 	public Bratchyk getBratchyk(long id){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "getBratchyk");
 		map.put("access_token", access_token);
-		map.put("act", "get");
 		map.put("id", id);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk.makeCopy(new Bratchyk(Parser.parse(s)));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (Bratchyk) command.execute(true);
 	}
 	public Bratchyk[] getBratchykHistory(long id){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "getBratchykHistory");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
 		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (Bratchyk[]) command.execute(true);
 	}
 	public User[] getUserList(){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "getUserList");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
-		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (User[]) command.execute(true);
 	}
 	public User getUser(long id){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "getUser");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
 		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return null;
+		}
+		return (User) command.execute(true);
 	}
 	public boolean setBratchyk(Bratchyk bratchyk, long id){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "setBratchyk");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
+		map.put("bratchyk", bratchyk);
 		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 	public boolean setUser(User user, long id){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "setUser");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
+		map.put("user", user);
 		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 	public boolean addUser(User user){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "addUser");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
-		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		map.put("user", user);
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 	public boolean addBratchyk(Bratchyk bratchyk){
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "addBratchyk");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
-		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		map.put("bratchyk", bratchyk);
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 	public boolean deleteBratchyk(long id) {
-		return false;
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "deleteBratchyk");
+		map.put("access_token", access_token);
+		map.put("id", id);
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 	public boolean deleteBratchykHistory(long id, int part) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "deleteBratchykHistory");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
 		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		map.put("part", part);
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 	public boolean deleteUser(long id) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("act", "deleteUser");
 		map.put("access_token", access_token);
-		map.put("act", "getversion");
 		map.put("id", id);
-		map.put("history", version);
-		final Bratchyk bratchyk = new Bratchyk();
-		connection.send(Parser.unparse(map), new StringResultHandler() {
-			
-			@Override
-			void handle(String s) throws Exception {
-				bratchyk = new Bratchyk(Parser.parse(s));
-			}
-		});
-		return bratchyk;
+		Command command;
+		try {
+			command = new Command(map);
+		} catch (Exception e) {
+
+			return false;
+		}
+		return (Boolean) command.execute(false);
 	}
 }
 
--- src/com/sheremet/utils/Command.java
+++ src/com/sheremet/utils/Command.java
@@ -1,5 +1,38 @@
 package com.sheremet.utils;
 
+import java.util.HashMap;
+
 public class Command {
-	
+
+
+	public Command(HashMap<String, Object> map) throws Exception {
+		String commandtype = (String) map.get("act");
+		switch (commandtype) {
+		case 	"value":
+		case 	"login":
+		case	"getBratchykChildren":
+		case	"getHeadBratchyks":
+		case	"getBratchyk":
+		case	"getBratchykHistory":
+		case	"getUserList":
+		case	"getUser":
+		case	"setBratchyk":
+		case	"setUser":
+		case	"addUser":
+		case	"addBratchyk":
+		case	"deleteBratchyk":
+		case	"deleteBratchykHistory":
+		case	"deleteUser":
+			break;
+
+		default:
+			throw new Exception();
+		}
+	}
+
+	public Object execute(boolean cacheble) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
