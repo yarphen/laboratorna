d73743e20cfe54be6867cc0de513f88206becff0
yarphen <leviatan.ka14@gmail.com>
2015-05-05 11:21:09 +0300

    Cache & mail confirm

---
+++ src/com/sheremet/utils/Commands.java
@@ -0,0 +1,19 @@
+package com.sheremet.utils;
+
+public enum Commands {
+ 	regRequest, 
+ 	login,
+	getBratchykChildren,
+	getHeadBratchyks,
+	getBratchyk,
+	getBratchykHistory,
+	getUserList,
+	getUser,
+	setBratchyk,
+	setUser,
+	addUser,
+	addBratchyk,
+	deleteBratchyk,
+	deleteBratchykHistory,
+	deleteUser
+}
