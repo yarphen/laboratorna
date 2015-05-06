711b94999f3021d615b60d7bcdfb8de222d27f16
yarphen <leviatan.ka14@gmail.com>
2015-05-06 11:56:17 +0300

    filtrating info function

--- src/com/sheremet/server/SecurityManager.java
+++ src/com/sheremet/server/SecurityManager.java
@@ -6,6 +6,7 @@ import com.sheremet.utils.Bratchyk;
 import com.sheremet.utils.User;
 
 public class SecurityManager {
+	DBWrappedAPI api;
 	public SecurityManager() {
 		// load database
 		// and init
@@ -13,17 +14,19 @@ public class SecurityManager {
 	public boolean tryToDo(HashMap<String, Object> command){
 		return true;
 	}
-	public User publicFiltrateUser(User u) {
+	public User filtrateUser(User u) {
 		u = new User(u);
 		u.passhash = null;
 		return u;
 	}
-	public Bratchyk publicFiltrateBratchyk(Bratchyk b) {
-		b = new Bratchyk(b);
-		b.kontakty = null;
-		b.datanarodzhennia = null;
-		b.datavysviaty = null;
-		b.version_id = null;
+	public Bratchyk filtrateBratchyk(Bratchyk b, String access_token) {
+		if (api.getPermissionOfTheToken(access_token)>1){
+			b = new Bratchyk(b);
+			b.kontakty = null;
+			b.datanarodzhennia = null;
+			b.datavysviaty = null;
+			b.version_id = null;
+		}
 		return b;
 	}
 }
