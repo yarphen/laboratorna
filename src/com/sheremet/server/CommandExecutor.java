aa55458662188e9e3e2982d06d73c70c1edd8b70
yarphen <leviatan.ka14@gmail.com>
2015-05-06 14:54:51 +0300

    creating command executor

---
+++ src/com/sheremet/server/CommandExecutor.java
@@ -0,0 +1,11 @@
+package com.sheremet.server;
+
+import com.sheremet.utils.Command;
+
+public class CommandExecutor {
+	public Object execute(Command command) {
+		return command;
+		// TODO Auto-generated method stub
+
+	}
+}
