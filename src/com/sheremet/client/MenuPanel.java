d879d44ed7e9b807ef78789c1842b477cad0670b
yarphen <leviatan.ka14@gmail.com>
2015-05-08 01:05:05 +0300

    add menu panel

---
+++ src/com/sheremet/client/MenuPanel.java
@@ -0,0 +1,11 @@
+package com.sheremet.client;
+
+import javax.swing.JPanel;
+
+public class MenuPanel extends JPanel {
+
+	public MenuPanel(ClientFrame clientFrame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
