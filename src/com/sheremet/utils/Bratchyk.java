81769d8fdce366603dbb9fefb5ccceae85de29e6
tsyapa <tsyapper14@gmail.com>
2015-05-13 15:20:01 +0300

    restore right alphabet order of fields

--- src/com/sheremet/utils/Bratchyk.java
+++ src/com/sheremet/utils/Bratchyk.java
@@ -26,9 +26,6 @@ public class Bratchyk {
 		}
 	}
 	public Bratchyk(Bratchyk b) {
-		prizvysche = b.prizvysche;
-		imya = b.imya;
-		pobatkovi = b.pobatkovi;
 		dataankety = b.dataankety;
 		datanarodzhennia = b.datanarodzhennia;
 		dataopatronennia = b.dataopatronennia;
@@ -36,12 +33,12 @@ public class Bratchyk {
 		dataversii=b.dataversii;
 		datavysviaty = b.datavysviaty;
 		id = b.id;
-		
+		imya = b.imya;
 		kontakty = b.kontakty;
 		patron_id = b.patron_id;
-		
+		pobatkovi = b.pobatkovi;
 		posady = b.posady;
-		
+		prizvysche = b.prizvysche;
 		rikvstupu = b.rikvstupu;
 		rikvypusku = b.rikvypusku;
 		specialnist = b.specialnist;
