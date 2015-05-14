5604f9b0517d08e10310030ce7174344553307bd
yarphen <leviatan.ka14@gmail.com>
2015-05-14 11:55:07 +0300

    correcting init block

--- src/com/sheremet/client/TreeViewPanel.java
+++ src/com/sheremet/client/TreeViewPanel.java
@@ -8,15 +8,14 @@ import javax.swing.JScrollPane;
 
 public class TreeViewPanel extends JPanel{
 	private SBTree tree;
-	private JPanel bratchykView;
+	private BratchykViewPanel bratchykView;
 	private Object[] root;
 	public TreeViewPanel(DBSecureAPI api){ 
 		setName("Name");
 		setSize(WIDTH, HEIGHT);
-		String result = "";
-		SBTree tree = new SBTree(api);
+		tree = new SBTree(api);
 		add(new JScrollPane(tree));
-		BratchykViewPanel panel = new BratchykViewPanel();
+		bratchykView = new BratchykViewPanel();
 	} 
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 600;
